8f911f21f6ac6562a170db94451ff895ffef1a27
frosDfurret <64030476+frosDfurret@users.noreply.github.com>
2022-02-15 03:12:04 +0000

    please just push

---
+++ assets/js/construction.js
@@ -0,0 +1,14 @@
+image = document.getElementsByTagName('img')[0]
+image.addEventListener("click", flash);
+flashing = false
+image.setAttribute('draggable', false);
+
+function flash() {
+  if (flashing == false) {
+    image.src = "/assets/media/under-construction.gif"
+    flashing = true
+  } else {
+    image.src = "/assets/media/under-construction-idle.png"
+    flashing = false
+  }
+}
