6e8fcbbf75f69f595e0756aefce2bd96133536b1
daguil <daniel.john.aguilar@gmail.com>
2022-02-17 23:08:04 +0000

    Added fallback page

---
+++ assets/js/nobile.js
@@ -0,0 +1,7 @@
+if (window.matchMedia("only screen and (max-width: 760px)").matches && window.location['pathname'] == "/home.html") {
+  window.location = "/m/"
+}
+
+if (!window.matchMedia("only screen and (max-width: 760px)").matches && window.location['pathname'] == "/m/") {
+  window.location = "/"
+}
